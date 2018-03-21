bcae2a9ef0011f029e936ca60aebc4b3f9556d32
jasonjsyuan <jasonjsyuan@tencent.com>
2018-03-21 14:38:12 +0800

    init

---
+++ parser.go
@@ -0,0 +1,27 @@
+package hotconfig
+
+import (
+	"rlog"
+	"encoding/json"
+	"io/ioutil"
+)
+
+func Parse(filename string, out interface{}) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		rlog.Errorln("read config file error[%s], filename[%s]",
+			err.Error(), filename)
+		return err
+	}
+
+	err = json.Unmarshal(file, out)
+
+	if err != nil {
+		rlog.Debugln("unmarshal config json file error[%s]", err.Error())
+		return err
+	}
+
+	rlog.Errorf("load config file finished, config[%+v]", out)
+
+	return nil
+}
